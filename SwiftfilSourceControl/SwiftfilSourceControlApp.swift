32662ef4f9d5d792ae32dbabcb7934541087cc1f
Denis <deniska.shabanov.2019@mail.ru>
2025-07-12 23:07:47 +0300

    Initial Commit

---
+++ SwiftfilSourceControl/SwiftfilSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfilSourceControlApp.swift
+//  SwiftfilSourceControl
+//
+//  Created by Denis Shabanov on 12.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfilSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftfilSourceControlTests/SwiftfilSourceControlTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfilSourceControlTests.swift
+//  SwiftfilSourceControlTests
+//
+//  Created by Denis Shabanov on 12.07.2025.
+//
+
+import Testing
+@testable import SwiftfilSourceControl
+
+struct SwiftfilSourceControlTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
