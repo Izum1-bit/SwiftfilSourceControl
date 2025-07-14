8249e3d0c26809293b18fef553e25a96016b2941
Denis <deniska.shabanov.2019@mail.ru>
2025-07-14 22:21:50 +0300

    [Feature] Fix Swiftful again

--- SwiftfilSourceControl/ContentView.swift
+++ SwiftfilSourceControl/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "heart.fill")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("SwiftFul Thinking!")
+            Text("SwiftFul!")
             
             Button("Subscribe now!"){
                 
