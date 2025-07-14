0de182e824e57d678730129371d275bb3893d1b8
Denis <deniska.shabanov.2019@mail.ru>
2025-07-14 14:11:48 +0300

    Add HomeView

---
+++ SwiftfilSourceControl/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  SwiftfilSourceControl
+//
+//  Created by Denis Shabanov on 14.07.2025.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Screen two")
+    }
+}
+
+#Preview {
+    HomeView()
+}
